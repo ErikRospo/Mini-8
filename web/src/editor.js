f12aff1f3d6c7284a955289d7543b00f2f64a9e0
ErikRospo <87732519+ErikRospo@users.noreply.github.com>
2025-07-08 19:48:54 -0700

    Add inlay hints for values

--- web/src/editor.js
+++ web/src/editor.js
@@ -1,5 +1,5 @@
 import { editor, languages } from "monaco-editor";
-import { OPCODES, REGISTERS,encodeOpcode,handleShorthand } from "./assembler";
+import { OPCODES, REGISTERS, encodeOpcode, handleShorthand } from "./assembler";
 export default function init_mini8() {
   languages.register({ id: "mini-8" });
   languages.setMonarchTokensProvider("mini-8", {
@@ -111,12 +111,89 @@ export default function init_mini8() {
         label: "define",
         kind: languages.CompletionItemKind.Keyword,
         insertText: "define ${1:name} ${2:value}",
-        insertTextRules:
-          languages.CompletionItemInsertTextRule.InsertAsSnippet,
+        insertTextRules: languages.CompletionItemInsertTextRule.InsertAsSnippet,
         documentation: "Define a constant",
       });
       return { suggestions };
     },
   });
 
+  languages.registerInlayHintsProvider("mini-8", {
+    provideInlayHints: (model, range) => {
+      const hints = [];
+      const lines = model.getLinesContent();
+      for (let i = range.startLineNumber - 1; i < range.endLineNumber; i++) {
+        const line = lines[i];
+
+        const instruction = line.match(/^\s*([A-Z]+)\b/);
+        let isWRT = false;
+        if (instruction) {
+          const mnemonic = instruction[1];
+          if (mnemonic === "WRT") {
+            isWRT = true;
+          }
+        }
+        //parse immediate values, and inlay the raw integer value
+        const hexMatch = line.match(/0x([0-9a-fA-F]+)/);
+        if (hexMatch) {
+          const hexValue = parseInt(hexMatch[1], 16);
+          if (hexValue < 10){
+            // You can parse hex values up to 10, right?
+            continue;
+          }
+          hints.push({
+            position: {
+              lineNumber: i + 1,
+              column: hexMatch.index + hexMatch[0].length + 1,
+            },
+            label: `: ${hexValue.toString()}`,
+            kind: languages.InlayHintKind.Parameter,
+          });
+        }
+        const binMatch = line.match(/0b([01]+)/);
+        if (binMatch) {
+          const binValue = parseInt(binMatch[1], 2);
+          if (isWRT && binValue < 4){
+            //This is probably the second argument of a WRT instruction, the formatter. Instead of showing the raw value, we show the formatter name
+            const formatters = ["utf", "dec","alph","hex"];
+            const formatter= formatters[binValue];
+            hints.push({
+              position: {
+                lineNumber: i + 1,
+                column: binMatch.index + binMatch[0].length + 1,
+              },
+              label: `: ${formatter}`,
+              kind: languages.InlayHintKind.Parameter,
+            });
+            continue;
+          }
+          if (binValue < 2) {
+            // You can parse bin values up to 2, right?
+            continue;
+          }
+          hints.push({
+            position: {
+              lineNumber: i + 1,
+              column: binMatch.index + binMatch[0].length + 1,
+            },
+            label: `: ${binValue.toString()}`,
+            kind: languages.InlayHintKind.Parameter,
+          });
+        }
+        const charMatch = line.match(/"[^\"', ]"/);
+        if (charMatch) {
+          const charValue = charMatch[0].slice(1, -1); // Remove quotes
+          hints.push({
+            position: {
+              lineNumber: i + 1,
+              column: charMatch.index + 4,
+            },
+            label: `: ${charValue.charCodeAt(0).toString()}`,
+            kind: languages.InlayHintKind.Parameter,
+          });
+        }
+      }
+      return { hints, dispose: () => {} };
+    },
+  });
 }
